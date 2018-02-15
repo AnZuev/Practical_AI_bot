3e81d6b9f479203323c46ecedffb0a5298175fb2
Maxim Kostin <x035cm@mail.ru>
2018-02-15 11:50:44 +0300

    Few changes and "bugfixing"
    Now first methot requires locale ("en_US" or "ru_RU") as input. returns text if found, else None.

--- update2text.py
+++ update2text.py
@@ -6,33 +6,31 @@ from bs4 import BeautifulSoup
 YANDEX_API_KEY="key" # put your key here
 
 
-def update2text(bot, update):
+def update2text(update, locale): # locale="ru_RU" or "en_US"
     message=update.message
 
     text=bot.get_file(message.text) # если в сообщении есть текст, то берём его для начала
 
-    if message.voice is not None: # если есть голос, то попробуем его распознать
+    if message.voice!=None: # если есть голос, то попробуем его распознать
         file_info = bot.get_file(message.voice.file_id) 
         file = requests.get(
-            'https://api.telegram.org/file/bot{0}/{1}'.format(YANDEX_API_KEY, file_info.file_path)) # вроде так из телеги файлы выкачиваются
+            'https://api.telegram.org/file/bot{0}/{1}'.format(BOT_API_TOKEN, file_info.file_path)) # вроде так из телеги файлы выкачиваются
         
         UUID = str(uuid.uuid4()).replace("-", "")
-        answer = speech_2_text(file.content, UUID) # пробуем распознать
+        answer = speech_2_text(file.content, UUID, locale) # пробуем распознать
 
         if len(answer)!=0:
             mv=max(answer, key=answer.get) # если удалось распознать речь, то берём лучшее совпадение
             text=mv 
 
-    if len(text)==0:
-        text="What? Moia tvoia ne ponimat. Try again!" # если в сообщении нет текста или не удалось распознать текст.
+    if text==None or len(text)==0:
+        text=None # если в сообщении нет текста или не удалось распознать текст.
     return text
 
 
-# this is unique idenifier of user
-# API assumes that APPLICATION makes requests and we can mark them
 
 
-def speech_2_text(data, uid, lang="ru_RU"):
+def speech_2_text(data, uid, lang):
     
     url = "https://asr.yandex.net/asr_xml?uuid={}&key={}&topic={}&lang={}&disableAntimat={}"
     url = url.format(uid, YANDEX_API_KEY, "queries", lang, "true")
