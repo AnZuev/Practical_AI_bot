efeb646b621f9437080f274a24df66e4dbeed43e
AndrewF <bnbyar@gmail.com>
2018-02-15 19:29:36 +0300

    translator fix

--- Translator/Translator.py
+++ Translator/Translator.py
@@ -1,23 +1,27 @@
 from Activity import Activity
+import locale
+from yandex import Translater
+from update2text import update2text
+from telegram import ReplyKeyboardMarkup as rkm
+from AI_bot import BOT_API_TOKEN
 
 # pip install yandex-translater
 
-class Translator(Activity):
-import locale
-from yandex import Translater
-from .. import update2text.update2text
-
-locale.setlocale(locale.LC_ALL, '')
 YANDEX_API_KEY="877f02a7-6e01-494e-bb36-b999b189f036"
+
+class Translator(Activity):
 
 
     def __init__(self):
-		self.translator = Translater()
-		self.translator.set_key(YANDEX_API_KEY)
-		self.mode='EN-->RU'
+        global YANDEX_API_KEY
+
+        locale.setlocale(locale.LC_ALL, '')
+        self.translator = Translater()
+        self.translator.set_key(YANDEX_API_KEY)
+        self.mode='EN-->RU'
 
     def first_query(self, bot, update):
-		self.__init__()
+        self.__init__()
         self.mode = rkm([['EN-->RU'], ['RU-->EN']])
         bot.sendMessage(
             chat_id=update.message.chat.id,
@@ -26,25 +30,25 @@ YANDEX_API_KEY="877f02a7-6e01-494e-bb36-b999b189f036"
         )
 
     def process(self, query, bot, update):
-		result=""
-		
+        result=""
+
         if query=='RU-->EN':
-			self.translator.set_from_lang('ru')
-			self.translator.set_to_lang('en')
-			ans=update2text(update, BOT_API_TOKEN, "ru-RU")
-			if ans!=None:
-				self.translator.set_text(ans)
-				result=self.translator.translate()
-			
-		elif query=='EN-->RU':
-			self.translator.set_from_lang('en')
-			self.translator.set_to_lang('ru')
-			ans=update2text(update, BOT_API_TOKEN, "en-US")
-			if ans!=None:
-				self.translator.set_text(ans)
-				result=self.translator.translate()
-				
-		if len(result)==0:
-			result="What? try again, keep calm speak slowly and clearly."
-			
-		return result
+            self.translator.set_from_lang('ru')
+            self.translator.set_to_lang('en')
+            ans=update2text(update, BOT_API_TOKEN, "ru-RU")
+            if ans!=None:
+                self.translator.set_text(ans)
+                result=self.translator.translate()
+
+        elif query=='EN-->RU':
+            self.translator.set_from_lang('en')
+            self.translator.set_to_lang('ru')
+            ans=update2text(update, BOT_API_TOKEN, "en-US")
+            if ans!=None:
+                self.translator.set_text(ans)
+                result=self.translator.translate()
+
+        if len(result)==0:
+            result="What? try again, keep calm speak slowly and clearly."
+
+        return result
